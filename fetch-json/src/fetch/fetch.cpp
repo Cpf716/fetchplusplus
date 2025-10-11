799eec2b63250e2a62024749cde44c717bd8bb11
Corey Ferguson <cpf716@gmail.com>
2025-10-10 23:12:12 -0400

    bug-30

--- fetch-json/src/fetch/fetch.cpp
+++ fetch-json/src/fetch/fetch.cpp
@@ -92,7 +92,7 @@ namespace fetch {
             headers.erase(key);
             
             // Map host
-            ss << "host: " << host;
+            ss << "host: " << host << "\r\n";
 
             // Generate content-length, if required
             key = "content-length";
@@ -105,19 +105,19 @@ namespace fetch {
             }       
 
             // Map request headers
-            for (const auto& [key, value]: headers) {
-                ss << "\r\n";
-                ss << key << ": " << value.str();
-            }
+            for (const auto& [key, value]: headers)
+                ss << key << ": " << value.str() << "\r\n";
 
             headers["host"] = host;
             // End - Map request headers
 
             // Map body
             if (body.length()) {
-                ss << "\r\n\r\n";
-                ss << body;
+                ss << "\r\n";
+                ss << body << "\r\n";
             }
+
+            ss << "\r\n";
 
             this->_message = ss.str();
 
